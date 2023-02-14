1bb2e57dd07bda0418e49b9e80f2eb09203794cb
ilyajob05 <ilyajob05@gmail.com>
2023-02-15 02:24:44 +0300

    Update xnor_net.py

--- xnor_net.py
+++ xnor_net.py
@@ -1,11 +1,197 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.nn.modules.utils import _single, _pair, _triple, _reverse_repeat_tuple
 
 from torchvision.datasets import MNIST, CIFAR10
-from torchvision.transforms import Compose, PILToTensor, Lambda
+from torchvision.transforms import Compose, PILToTensor, ToTensor, Lambda, transforms
 
 from tqdm.notebook import tqdm
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+t1 = torch.ones(3, 3, device = device)
+print(t1)
+
+
+class BinaryActivationFunc(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input: torch.Tensor) -> torch.Tensor:
+        ctx.save_for_backward(input)
+        result = torch.sign(input)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input.le(-1)] = 0
+        grad_input[input.ge(-1)] = 0
+        return grad_input
+
+
+class BinaryConv2d(torch.nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride = (1,1), padding = (0,0), dilation = (1,1), groups = 1,
+                 bias = True, padding_mode: str = 'zeros', device = None, dtype = None, out_width = 1, out_height = 1) -> None:
+        super(BinaryConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups,
+                                           bias, padding_mode, device, dtype)
+        self.factory_kwargs = {'device': device, 'dtype': dtype}
+        if bias:
+            self.bias = torch.autograd.Parameter(torch.empty(out_channels, **(self.factory_kwargs)))
+        else:
+            self.register_parameter('bias', None)
+        self.stride = _pair(stride)
+        self.padding = padding if isinstance(padding, str) else _pair(padding)
+        self.dilation = _pair(dilation)
+        self.groups = groups
+
+        self.shape = (out_channels, in_channels, kernel_size, kernel_size)
+        self.weight = nn.Parameter(torch.rand(self.shape) * 0.001, requires_grad=True)
+        self.alpha = nn.Parameter(torch.ones(out_height).reshape(1,-1,1), requires_grad=False)
+        self.beta = nn.Parameter(torch.ones(out_width).reshape(1,1,-1), requires_grad=False)
+        self.gamma = nn.Parameter(torch.ones(out_channels).reshape(-1,1,1), requires_grad=False)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        input = BinaryActivationFunc.apply(input)
+        real_weight = self.weight
+        mean_weights = real_weight.mul(-1).mean(dim=1, keepdim=True).expand_as(self.weight).contiguous()
+        centered_weights = real_weight.add(mean_weights)
+        cliped_weights = torch.clamp(centered_weights, -1.0, 1.0)
+        signed_weights = torch.sign(centered_weights).detach() - cliped_weights.detach() + cliped_weights
+        binary_weights = signed_weights
+        input = torch.nn.functional.conv2d(input, binary_weights, bias=self.bias, stride=self.stride, padding=self.padding,
+                                           dilation=self.dilation, groups=self.groups)
+        return input.mul(self.gamma).mul(self.beta).mul(self.alpha)
+
+
+
+# def BinaryConvolution(
+#         in_channels,
+#         out_channels,
+#         kernel_size,
+#         stride = 1,
+#         padding = 0,
+#         dilation = 1,
+#         groups = 1,
+#         bias = True,
+#         padding_mode: str = 'zeros',  # TODO: refine this type
+#         device=None,
+#         dtype=None
+# ):
+#     sq = torch.nn.Sequential()
+#     sq.add_module('input', BinaryActivationFunc(nn.BatchNorm2d(in_channels, eps=1.0e-4)))
+#     # sq.add_module('bin_activation', BinaryActivationFunc())
+#     sq.add_module('conv', nn.Conv2d(in_channels, out_channels,kernel_size,stride,padding,dilation, groups, bias, padding_mode, device, dtype))
+#     return sq
+
+
+def BinaryMaxConvolution(
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride = 1,
+        padding = 0,
+        dilation = 1,
+        groups = 1,
+        bias = True,
+        padding_mode: str = 'zeros',  # TODO: refine this type
+        device=None,
+        dtype=None
+):
+    sq = torch.nn.Sequential()
+    sq.add_module('input', nn.BatchNorm2d(in_channels, eps=1.0e-4))
+    sq.add_module('bin_activation', BinaryActivationFunc())
+    sq.add_module('conv', nn.Conv2d(in_channels, out_channels,kernel_size,stride,padding,dilation, groups, bias, padding_mode, device, dtype))
+    sq.add_module('max_pool', nn.MaxPool2d(3,2))
+    return sq
+
+
+class NetBin(nn.Module):
+    def __init__(self):
+        super(NetBin, self).__init__()
+        # convolution 1
+        self.bn0 = nn.BatchNorm2d(3)
+        self.conv00 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu00 = nn.PReLU()
+        self.conv0 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu0 = nn.PReLU()
+        self.pool0 = nn.MaxPool2d(2)
+        self.drop0 = nn.Dropout2d()
+
+        # convolution 2 bin
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv10 = BinaryConv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu10 = nn.PReLU()
+        self.conv1 = BinaryConv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu1 = nn.PReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.drop1 = nn.Dropout2d()
+
+        # convolution 3
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv20 = BinaryConv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu20 = nn.PReLU()
+        self.conv2 = BinaryConv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu2 = nn.PReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.drop2 = nn.Dropout2d()
+
+        # full connection layer 1
+        self.bn3 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128 * 4 * 4, 10)
+        self.fc1Prelu = nn.PReLU()
+
+        # full connection layer 2
+        self.fc2 = nn.Linear(10, 10)
+
+    # direct computation
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.conv00(x)
+        x = self.relu00(x)
+        x = self.conv0(x)
+        x = self.relu0(x)
+        x = self.pool0(x)
+        x = self.drop0(x)
+
+        x = self.bn1(x)
+        x = self.conv10(x)
+        x = self.relu10(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.drop1(x)
+
+        x = self.bn2(x)
+        x = self.conv20(x)
+        x = self.relu20(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.drop2(x)
+
+        #         show input parameter
+        #         print(x.shape)
+        x = self.bn3(x)
+        x = self.fc1(x.view(x.size(0), -1))
+        x = self.fc1Prelu(x)
+        x = torch.nn.functional.dropout(x)
+        x = self.fc2(x)
+        x = torch.nn.functional.softmax(x, dim=1)
+        return x
+
+
+# network initialization
+net_bin = NetBin().to(device)
+# show network
+print(net_bin)
+
+
+
+
+
+
+
+
 
 
 class BinaryLinearFunc(torch.autograd.Function):
@@ -105,10 +291,8 @@ def create_net(n_hidden=4, input_dim=784, out_dim=10, hid_dim=128, binary=False)
 
     return net
 
-tfm = Compose([
-    PILToTensor(),
-    Lambda(lambda x: x.flatten() / 255),
-])
+normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+tfm = Compose([ToTensor(), normalize, ])
 
 # train_data = MNIST("mnist", train=True, download=True, transform=tfm)
 # test_data = MNIST("mnist", train=False, download=True, transform=tfm)
@@ -128,31 +312,35 @@ writer = SummaryWriter('logs')
 
 def train_epoch(net, optim, loss_fn, epoch, step, log_name):
     for image, label in train_loader:
-        image = image
-        label = label
+        image = image.to(device)
+        label = label.to(device)
 
         output = net(image)
         loss = loss_fn(output, label)
-        writer.add_hparams({'alpha_0': net.hidden_0.alpha.data.item(),
-                            'betta_0': net.hidden_0.betta.data.sum().item(),
-                            'gamma_0': net.hidden_0.betta.data.sum().item(),
-                            'alpha_1': net.hidden_1.alpha.data.item(),
-                            'betta_1': net.hidden_1.betta.data.sum().item(),
-                            'gamma_1': net.hidden_1.betta.data.sum().item(),
-                            'alpha_2': net.hidden_2.alpha.data.item(),
-                            'betta_2': net.hidden_2.betta.data.sum().item(),
-                            'gamma_2': net.hidden_2.betta.data.sum().item(),
-                            'alpha_3': net.hidden_3.alpha.data.item(),
-                            'betta_3': net.hidden_3.betta.data.sum().item(),
-                            'gamma_3': net.hidden_3.betta.data.sum().item()},
-                           {f"Loss/train{log_name}": loss.item()})
+
         writer.add_scalars(f"Loss/train", {log_name: loss.item()}, step[0])
 
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0e-4)
         optim.step()
         optim.zero_grad()
         step[0] += 1
         writer.flush()
+    writer.add_hparams({'alpha_0': net.hidden_0.alpha.data.item(),
+                        'betta_0': net.hidden_0.betta.data.sum().item(),
+                        'gamma_0': net.hidden_0.betta.data.sum().item(),
+                        'alpha_1': net.hidden_1.alpha.data.item(),
+                        'betta_1': net.hidden_1.betta.data.sum().item(),
+                        'gamma_1': net.hidden_1.betta.data.sum().item(),
+                        'alpha_2': net.hidden_2.alpha.data.item(),
+                        'betta_2': net.hidden_2.betta.data.sum().item(),
+                        'gamma_2': net.hidden_2.betta.data.sum().item(),
+                        'alpha_3': net.hidden_3.alpha.data.item(),
+                        'betta_3': net.hidden_3.betta.data.sum().item(),
+                        'gamma_3': net.hidden_3.betta.data.sum().item()},
+                       {f"Loss/train{log_name}": loss.item()})
+
+    writer.add_histogram()
 
 
 @torch.no_grad()
@@ -164,8 +352,8 @@ def tst_epoch(net, loss_fn, epoch, step, log_name):
     total_samples = 0
 
     for image, label in test_loader:
-        image = image
-        label = label
+        image = image.to(device)
+        label = label.to(device)
 
         output = net(image)
         loss = loss_fn(output, label)
@@ -181,10 +369,10 @@ def tst_epoch(net, loss_fn, epoch, step, log_name):
 
 
 
-bin_net = create_net(input_dim=input_dim, hid_dim=1024, binary=True)
+bin_net = create_net(input_dim=input_dim, hid_dim=1024, binary=True).to(device)
 
-optim_l = torch.optim.RAdam(bin_net.parameters(), lr=1.0e-5)
-optim = torch.optim.RAdam(bin_net.parameters(), lr=5.0e-5)
+optim_l = torch.optim.RAdam(net_bin.parameters(), lr=1.0e-5)
+optim = torch.optim.RAdam(net_bin.parameters(), lr=5.0e-5)
 # optim = torch.optim.ASGD(bin_net.parameters(), lr=1e-4)
 loss_fn = nn.CrossEntropyLoss()
 
@@ -198,21 +386,17 @@ max_t = 2
 
 for epoch in range(n_epochs):
     if epoch == 0:
-        train_epoch(bin_net, optim_l, loss_fn, epoch, step_train, 'bin')
-        tst_epoch(bin_net, loss_fn, epoch, step_test, 'bin')
+        train_epoch(net_bin, optim_l, loss_fn, epoch, step_train, 'bin')
+        tst_epoch(net_bin, loss_fn, epoch, step_test, 'bin')
 
-    train_epoch(bin_net, optim, loss_fn, epoch, step_train, 'bin')
-    tst_epoch(bin_net, loss_fn, epoch, step_test, 'bin')
-
-print('end')
+    train_epoch(net_bin, optim, loss_fn, epoch, step_train, 'bin')
+    tst_epoch(net_bin, loss_fn, epoch, step_test, 'bin')
 
 
 
-
-simple_net = create_net(input_dim=input_dim)
+simple_net = create_net(input_dim=input_dim).to(device)
 
 optim = torch.optim.Adam(simple_net.parameters(), lr=1e-3)
-
 step_train = [0]
 step_test = [0]
 n_epochs = 10
@@ -224,4 +408,7 @@ for epoch in range(n_epochs):
 
 
 
+
+
+print('end')
 
